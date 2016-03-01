e7935ac0ace5a4c7c058bf58e989b4a337e222b8
feelyd <feelyd@amazon.com>
2016-03-01 13:21:17 +0000

    Fixed createsu

--- saleor/management/commands/createsu.py
+++ saleor/management/commands/createsu.py
@@ -5,6 +5,6 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username=os.getenv('SUPERUSER_NAME')).exists():
-            User.objects.create_superuser(os.getenv('SUPERUSER_NAME'), os.getenv('SUPERUSER_EMAIL'), os.getenv('SUPERUSER_PASS'))
+        if not User.objects.filter(email=os.getenv('SUPERUSER_EMAIL')).exists():
+            User.objects.create_superuser(email=os.getenv('SUPERUSER_EMAIL'), password=os.getenv('SUPERUSER_PASS'))
 
