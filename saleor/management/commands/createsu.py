f53f0723ffd3782c109f0e891298291d395d7fcd
feelyd <feelyd@amazon.com>
2016-03-01 12:46:01 +0000

    Use correct User model

--- saleor/management/commands/createsu.py
+++ saleor/management/commands/createsu.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from saleor.userprofile.models import User
 import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username=os.getenv('SUPERUSER_NAME')).exists():
             User.objects.create_superuser(os.getenv('SUPERUSER_NAME'), os.getenv('SUPERUSER_EMAIL'), os.getenv('SUPERUSER_PASS'))
 
